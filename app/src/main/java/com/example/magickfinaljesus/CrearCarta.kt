139638160ac190fe849220525ae75703c2bfbb6b
Survivor <jesusmartinezyo@hotmail.com>
2022-02-14 02:26:55 +0100

    DRAWER CONSEGUIDO SUUUUUU

--- app/src/main/java/com/example/magickfinaljesus/CrearCarta.kt
+++ app/src/main/java/com/example/magickfinaljesus/CrearCarta.kt
@@ -3,9 +3,9 @@ package com.example.magickfinaljesus
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class AdminMain : AppCompatActivity() {
+class CrearCarta : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_admin_main)
+        setContentView(R.layout.activity_crear_carta)
     }
 }
